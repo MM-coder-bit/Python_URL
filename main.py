79d0a2100cfccdc6ebd9287034c017506796ce00
Mateus <marques18@outlook.com>
2023-02-11 14:12:35 -0300

    adicionado método len(), responsável por mostrar o tamanho da string

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ print(url_base)
 # Fatiando a string do '?' até o final
 url_parametros = url[(i_interrogacao + 1):]
 print(url_parametros)
+
+# método len() para buscar o valor desejado
+parametro_busca = 'moedaOrigem'
+i_parametro = url_parametros.find(parametro_busca) # buscando na str principal, a str de busca
+i_valor = i_parametro + (len(parametro_busca) + 1) # encontrando o index inicial do valor desejado (y = x + (len(x) + 1))
+valor = url_parametros[i_valor:] # método de fatiamento
+print(valor)
