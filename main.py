234ef40b23b41082e7c76882782e6a11b983aff9
Mateus <marques18@outlook.com>
2023-02-11 14:38:30 -0300

    Adicionado lógica que busque o valor desejado indepedente da posição do '&'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 print(url)
 
 #--------------------------------------------------------------------------------------------------------#
@@ -13,14 +13,20 @@ i_interrogacao = url.find('?')
 # Fatiando a string até o '?'
 url_base = url[:i_interrogacao] 
 print(url_base)
-
 # Fatiando a string do '?' até o final
 url_parametros = url[(i_interrogacao + 1):]
 print(url_parametros)
 
 # método len() para buscar o valor desejado
-parametro_busca = 'moedaOrigem'
+parametro_busca = 'moedaDestino'
 i_parametro = url_parametros.find(parametro_busca) # buscando na str principal, a str de busca
 i_valor = i_parametro + (len(parametro_busca) + 1) # encontrando o index inicial do valor desejado (y = x + (len(x) + 1))
-valor = url_parametros[i_valor:] # método de fatiamento
+i_E_comercial = url_parametros.find('&',i_valor) # encontrando o '&' apartir do primeiro index da str desejada
+
+# Se retorna -1 significa que não há '&', então busque até o fim da linha
+if i_E_comercial == -1:
+    valor = url_parametros[i_valor:] # método de fatiamento
+# Senão retorna até o '&'
+else:
+    valor = url_parametros[i_valor:i_E_comercial]
 print(valor)
