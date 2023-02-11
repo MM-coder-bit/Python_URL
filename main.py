3d6c72dad31ddbba41e27402b1871642d00f36e4
Mateus <marques18@outlook.com>
2023-02-11 13:57:12 -0300

    adicionado método find(), responsável por buscar a string desejada

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ print(url)
 #--------------------------------------------------------------------------------------------------------#
 # O operador de fatiamento [a:b], utilizado para obter uma substring desde o índice a até o índice b - 1 
 # da string original. Lembrando que b - 1 pois o segundo argumento do fatiamento é exclusivo.
+# OBS.: se o fatiamento esta sem um dos argumentos, significa que inicia/finaliza até de/ate inicio/fim da linha [:B]-[B:]
 #--------------------------------------------------------------------------------------------------------#
 
+# Encontrando o '?'
+i_interrogacao = url.find('?')
+
 # Fatiando a string até o '?'
-url_base = url[0:19]
+url_base = url[:i_interrogacao] 
 print(url_base)
 
 # Fatiando a string do '?' até o final
-url_parametros = url[20:36]
+url_parametros = url[(i_interrogacao + 1):]
 print(url_parametros)
